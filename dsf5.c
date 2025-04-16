067cb27885dc5f99d934633fe5d17fd79c9d54e7
Cauã Sturaro <caua.sturaro111@gmail.com>
2025-04-16 14:04:10 -0300

    Create dsf5.c

---
+++ dsf5.c
@@ -0,0 +1,11 @@
+// int ->  your successor
+
+#include <stdio.h>
+
+int main(void) {
+	int num;
+	printf("digite um numero inteiro: ");
+	scanf("%i", &num);
+	
+	printf("Seu sucessor e: %i", num + 1);
+}
