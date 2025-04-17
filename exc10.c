7fbaec5af209da1b5237b0281513c07a695d251a
Cauã Sturaro <caua.sturaro111@gmail.com>
2025-04-17 01:26:05 -0300

    Update exc10.c

--- exc10.c
+++ exc10.c
@@ -10,12 +10,10 @@ int main(void) {
 
 	int b1, b2, b4, b8, b16;
 
-	b16 = (num/16) %2;
-	b8 = (num/8) % 2;
-	b4 = (num/4) % 2;
-	b2 = (num/2) % 2;
-	b1 = num %2;
+	int bin = ((num/16) % 2 * 10000) + ((num/8) % 2 * 1000) + ((num/4) % 2 * 100) + ((num/2) % 2 * 10) + (num %2 * 1);
 
-	printf("%i%i%i%i%i", b16, b8, b4, b2, b1);
+	printf("%i", bin);
 
+	return 0;
 }
+
