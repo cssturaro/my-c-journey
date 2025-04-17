50cb69216080650389903352982564271cabce3f
Cauã Sturaro <caua.sturaro111@gmail.com>
2025-04-16 23:56:00 -0300

    Create dsf12.c

---
+++ dsf12.c
@@ -0,0 +1,13 @@
+// m/s -> km/h
+
+#include <stdio.h>
+
+int main(void){
+	float speed;
+	
+	printf("Type the speed in m/s: /n> ");
+	scanf("%f", speed);
+	
+	printf("Your speed in km/h is: %f", speed *3,6);
+	
+}
