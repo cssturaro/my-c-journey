8104579f88d7d9a8d8e603c441020655a73cb3fe
Cauã Sturaro <caua.sturaro111@gmail.com>
2025-04-19 16:49:18 -0300

    Create exc21.c

---
+++ exc21.c
@@ -0,0 +1,16 @@
+// number -> is positive or not
+
+#include <stdio.h>
+
+int main(void){
+    int num;
+
+    printf("Type an integer: \n> ");
+    scanf("%i", &num);
+
+    if (num > 0){
+        printf("You typed a positive number");
+    } else {
+        printf("You typed a negative number");
+    }
+}
