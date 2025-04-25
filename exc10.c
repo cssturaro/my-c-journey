92d3ca8120fe6d3ba459be8c277a97c421f9a849
Cauã Sturaro <caua.sturaro111@gmail.com>
2025-04-25 08:12:22 -0300

    Update exc10.c

--- exc10.c
+++ exc10.c
@@ -1,17 +1,18 @@
-// int lower then 32 -> binary
-
 #include <stdio.h>
 
 int main(void) {
-	int num;
+    int num;
 
-	printf("Type your int: \n> ");
-	scanf("%i", &num);
+    printf("Type your int (0-31): \n> ");
+    scanf("%i", &num);
 
-	int bin = ((num/16) % 2 * 10000) + ((num/8) % 2 * 1000) + ((num/4) % 2 * 100) + ((num/2) % 2 * 10) + (num %2 * 1);
+    int bin = ((num / 16) % 2 * 10000) +
+              ((num / 8) % 2 * 1000) +
+              ((num / 4) % 2 * 100) +
+              ((num / 2) % 2 * 10) +
+              (num % 2 * 1);
 
-	printf("%i", bin);
+    printf("Binary: %05d\n", bin);
 
-	return 0;
+    return 0;
 }
-
